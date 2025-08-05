4144a682541cc4bec1a2bfaf0efdbdd6772f661a
Akeel <noormohommaduakeel@gmail.com>
2025-08-05 22:01:04 +0530

    transport backend

---
+++ backend/Transport/src/test/java/com/example/Transport/TransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
